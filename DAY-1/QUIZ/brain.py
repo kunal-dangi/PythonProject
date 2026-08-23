8d374c5b60c7024139e6f92b628679f189207657
kunal-dangi <kunaldangi065@gmail.com>
2026-08-23 22:06:47 +0530

    I am committed!!

--- DAY-1/QUIZ/brain.py
+++ DAY-1/QUIZ/brain.py
@@ -5,8 +5,7 @@ class QuestionBrain:
         self.score = 0
 
     def still_has_questions(self):
-        return self.question_number > len(self.question_list)
-
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
--- DAY-1/QUIZ/main.py
+++ DAY-1/QUIZ/main.py
@@ -4,8 +4,8 @@ from brain import QuestionBrain
 
 question_bank= []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -14,3 +14,5 @@ quiz = QuestionBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
 
+print("Quiz is completed")
+print(f"Your Final Score is {quiz.score}/{quiz.question_number}")
