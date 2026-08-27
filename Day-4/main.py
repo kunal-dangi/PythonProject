3e6477cfeeda0e32b6eb92b86ad6d9e05eaefd88
kunal-dangi <kunaldangi065@gmail.com>
2026-08-27 21:08:18 +0530

    I am committed!!

---
+++ Day-4/main.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+screen = Screen()
+screen.setup(width=600, height=500)
+screen.bgcolor("black")
+screen.title("Snake GAME")
+screen.tracer(0)
+
+# colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+
+segments = []
+
+
+snake = Snake()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+screen.exitonclick()
