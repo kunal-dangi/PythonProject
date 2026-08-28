8722a26126b6a3c85cb07cddf8d216b1a55a0d99
kunal-dangi <kunaldangi065@gmail.com>
2026-08-28 18:12:13 +0530

    commit

--- Day-4/main.py
+++ Day-4/main.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 import time
 from snake import Snake
 screen = Screen()
-screen.setup(width=600, height=500)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake GAME")
 screen.tracer(0)
