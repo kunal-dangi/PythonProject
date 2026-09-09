c27c9bd2b5468ad35137438570b5ab0ae527a5f2
kunal-dangi <kunaldangi065@gmail.com>
2026-09-09 22:20:04 +0530

    I am committed!!

--- Day-10/main.py
+++ Day-10/main.py
@@ -34,3 +34,35 @@ print(number2)
 
 common = [int(n) for n in number1 if n in number2]
 print(common)
+
+# # that's all for now!!
+
+
+
+
+
+# Well now lets look at {Dictionary Comprehension}
+# syntax
+import random
+
+students = ["Alex", "Steve", "Gork","Ladiaz", "Stu"]
+
+scores= {item:random.randint(1, 100) for item in students }
+# assigning random score to items of list students and finally it will return in a dictionary as we have item already
+# and have generated its value already, also covered in {},
+#so...
+print(scores)
+student_dict = {student:score for student, score in scores.items()}
+print(student_dict)
+
+# output -> {'Alex': 58, 'Steve': 33, 'Gork': 45, 'Ladiaz': 86, 'Stu': 21}
+
+# to loop through each row in data, we have a method in pandas
+# its syntax is like ->
+import pandas as pd
+student_list = list(student_dict.items())
+student_data_frame = pd. DataFrame(student_dict)
+print(student_data_frame)
+# actual line of looping
+for (index, row) in student_data_frame.iterrows():     # iterrows()... method for looping of rows in pandas
+    print(row.student, row.score)
