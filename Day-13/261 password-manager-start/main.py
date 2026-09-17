89b5c085b1ce2f8ef09d9d353a8784a32768b43c
kunal-dangi <kunaldangi065@gmail.com>
2026-09-17 19:58:47 +0530

    I am committed!!

--- Day-13/261 password-manager-start/main.py
+++ Day-13/261 password-manager-start/main.py
@@ -1,13 +1,49 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# PASSWORD GENERATOR->
+
 from tkinter import *
+from tkinter import messagebox as mbox, messagebox
+
 
 def save_password():
     web = input1.get()
     email = input2.get()
     password = input3.get()
-    with open("data.txt", "w") as f:
-        f.write(web + "  |  " + email + "  |  " + password + "\n")
 
+    if len(web) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showerror("Error", "Please enter all required information")
+    else:
+        is_ok = mbox.askokcancel(title="website",message = f"These are the details entered: \nEmail: {email} "
+                                                f"\nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "w") as f:
+                f.write(web + "  |  " + email + "  |  " + password + "\n")
+                input1.delete(0, END)
+                input3.delete(0, END)
+
+# Random Password Generator ->
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+password_letters = [random.choice(letters) for _ in range(nr_letters)]
+password_symbols = [random. choice(symbols) for _ in range(nr_symbols)]
+password_numbers = [random. choice(numbers) for _ in range(nr_numbers)]
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 
 # UI SETUP
 
