e85f9532f78bb08578e2a360883a2bad9b57eeab
kunal-dangi <kunaldangi065@gmail.com>
2026-09-20 16:51:27 +0530

    I am committed!!

--- Day-13/261 password-manager-start/main.py
+++ Day-13/261 password-manager-start/main.py
@@ -2,51 +2,52 @@
 
 from tkinter import *
 from tkinter import messagebox as mbox, messagebox
+import random
+import pyperclip
 
 
 def save_password():
     web = input1.get()
     email = input2.get()
-    password = input3.get()
+    passwordEntry = input3.get()
 
-    if len(web) == 0 or len(email) == 0 or len(password) == 0:
+    if len(web) == 0 or len(email) == 0 or len(passwordEntry) == 0:
         messagebox.showerror("Error", "Please enter all required information")
     else:
         is_ok = mbox.askokcancel(title="website",message = f"These are the details entered: \nEmail: {email} "
-                                                f"\nPassword: {password} \nIs it ok to save?")
+                                                f"\nPassword: {passwordEntry} \nIs it ok to save?")
 
         if is_ok:
-            with open("data.txt", "w") as f:
-                f.write(web + "  |  " + email + "  |  " + password + "\n")
+            with open("data.txt", "a") as f:
+                f.write(web + "  |  " + email + "  |  " + passwordEntry + "\n")
                 input1.delete(0, END)
                 input3.delete(0, END)
 
 # Random Password Generator ->
 
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def Generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-password_list = []
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
 
-password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
 
-password_symbols = [random. choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
 
-password_numbers = [random. choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
 
-random.shuffle(password_list)
+    password = "".join(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+    input3.insert(0, password)
+    pyperclip.copy(password)
 
-print(f"Your password is: {password}")
 
 # UI SETUP
 
@@ -80,7 +81,7 @@ input3 = Entry(window , width= 20, bd=3)
 input3.grid(column=1, row=3, sticky = "w")
 # sticky keeps it sticky to a direction in a column,,, columnspan increases the reach of widget to more than 1 column
 
-button = Button(window, text = "Generate Password", fg="black", font=("Arial", 25))
+button = Button(window, text = "Generate Password", fg="black", font=("Arial", 25), command=Generate_password)
 button.grid(column = 2, row=3, sticky = "w")
 
 button2 = Button(window, text = "Add", fg="black", font=("Arial", 25), width=40, command=save_password)
